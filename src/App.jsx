3f79839515fde803ffba58c2c1670ffda7267536
Melisa Lachev <melisatormac55@gmail.com>
2025-09-02 16:12:07 +0300

    initial syling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { characters } from "./characters.js";
-
+import "./App.css";
 export default function App() {
   return (
     <>
-      {characters.map((char) => (
-        <img src={char.src}></img>
-      ))}
+      <div className="card-container">
+        {characters.map((char) => (
+          <card>
+            <img
+              className="card-img"
+              src={char.src}
+              alt={"A picture of " + char.name}
+            ></img>
+            <div className="container">
+              <h3>{char.name}</h3>
+            </div>
+          </card>
+        ))}
+      </div>
     </>
   );
 }
