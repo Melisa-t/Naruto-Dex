1f69543bb7c4b10eb98ff8d2653d2d6cd0e301fc
Melisa Lachev <melisatormac55@gmail.com>
2025-09-02 18:17:25 +0300

    styled game over message

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import "./App.css";
 import { useState } from "react";
 export default function App() {
   const [characterList, setCharacterList] = useState(characters);
+
   const gameOver =
     characterList.some((char) => Object.hasOwn(char, `isClicked`)) &&
     characterList.some((char) => char.isClicked === false);
+
   const shuffle = (array, id) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
+
     setCharacterList(
       characterList.map((char) =>
         char.id === id ? { ...char, isClicked: !char.isClicked } : char
@@ -19,11 +22,9 @@ export default function App() {
   };
 
   const characterEl = characterList.map((char) => (
-    <div>
+    <div className="card-container">
       <img
-        onClick={() => {
-          shuffle(characterList, char.id);
-        }}
+        onClick={() => shuffle(characterList, char.id)}
         key={char.id}
         className="card-img"
         src={char.src}
@@ -39,19 +40,26 @@ export default function App() {
     <>
       {gameOver ? (
         <div className="game-over-msg">
-          <h1>
-            You clicked a character already clicked. <br></br>Try again!
-          </h1>
+          <p>
+            You clicked a character twice. <br></br> Try again!
+          </p>
           <button
-            onClick={() => {
-              setCharacterList(characters);
-            }}
+            onClick={() => setCharacterList(characters)}
+            className="retry-btn"
           >
-            Again!
+            Let's Go!
           </button>
         </div>
       ) : (
-        <div className="card-container">{characterEl}</div>
+        <>
+          <h1 className="primary-heading">Naruto Memo</h1>
+          <p className="introductory-text">
+            Welcome!
+            <br></br>
+            Choose each character only once. Let's see your memory game!
+          </p>
+          <div className="game-container">{characterEl}</div>
+        </>
       )}
     </>
   );
